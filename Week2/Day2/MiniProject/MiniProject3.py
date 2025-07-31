8fa8d27e8da7e107fc2b6b7b992e9e3b30869dad
ArielKoss <arielkossmann@gmail.com>
2025-07-31 22:57:07 +0300

    Add Tic Tac Toe game with player turns and win conditions

--- Week2/Day2/MiniProject/MiniProject3.py
+++ Week2/Day2/MiniProject/MiniProject3.py
@@ -4,9 +4,9 @@ def display_board(board):
     print("\nTIC TAC TOE")
     print("***************")
     print(f" {board[0]} | {board[1]} | {board[2]} ")
-    print("---+---+---")
+    print("---|---|---")
     print(f" {board[3]} | {board[4]} | {board[5]} ")
-    print("---+---+---")
+    print("---|---|---")
     print(f" {board[6]} | {board[7]} | {board[8]} ")
     print("***************\n")
 
