2c8681fff34df7f3504adf63d20af41fd4975fa7
ArielKoss <arielkossmann@gmail.com>
2025-07-31 21:10:33 +0300

    Add Tic Tac Toe game with player turns and win conditions

--- Week2/Day2/MiniProject/MiniProject3.py
+++ Week2/Day2/MiniProject/MiniProject3.py
@@ -1,25 +1,31 @@
 #MiniProject3.py
 
 def display_board(board):
-    print("---------")
-    for i in range(3):
-        print(f"| {board[i * 3]} | {board[i * 3 + 1]} | {board[i * 3 + 2]} |")
-        print("---------")
+    print("\nTIC TAC TOE")
+    print("***************")
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
+    print("***************\n")
 
 
 def player_input(player, board):
     while True:
         try:
-            position = int(input(f"Player {player}, choose a position (1-9): ")) - 1
-            if position < 0 or position >= 9:
-                print("Invalid position. Please choose a number between 1 and 9.")
-            elif board[position] != ' ':
-                print("This position is already taken. Try again.")
+            row = int(input(f"Player {player}'s turn...\nEnter row: ")) - 1
+            column = int(input(f"Enter column: ")) - 1
+            if 0 <= row <= 2 and 0 <= column <= 2:
+                if board[row * 3 + column] == ' ':
+                    board[row * 3 + column] = player
+                    break
+                else:
+                    print("This position is already taken. Try again.")
             else:
-                board[position] = player
-                break
+                print("Invalid input. Please enter values between 1 and 3 for both row and column.")
         except ValueError:
-            print("Invalid input. Please enter a number between 1 and 9.")
+            print("Invalid input. Please enter a valid number.")
 
 
 def check_win(board):
@@ -35,7 +41,7 @@ def check_win(board):
     ]
 
     for combo in winning_combinations:
-        if board[combo[0]] == board[combo[1]] == board[combo[2]] and board[combo[0]] != ' ':
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] != ' ':
             return board[combo[0]]
     return None
 
@@ -43,20 +49,19 @@ def check_win(board):
 def play():
     board = [' '] * 9
     current_player = 'X'
-    turns = 0
+    winner = None
+    turn_count = 0
 
-    while turns < 9:
+    while turn_count < 9 and winner is None:
         display_board(board)
         player_input(current_player, board)
-
         winner = check_win(board)
         if winner:
             display_board(board)
             print(f"Player {winner} wins!")
             break
-
         current_player = 'O' if current_player == 'X' else 'X'
-        turns += 1
+        turn_count += 1
 
     if winner is None:
         display_board(board)
@@ -64,4 +69,3 @@ def play():
 
 
 play()
-
